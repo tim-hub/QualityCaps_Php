dd122bea651d096fc9d9cfd115d455315917a8cd
tim-hub <tim_w@outlook.com>
2018-06-06 15:33:07 +1200

    change to picsum place holder, which is faster

--- database/factories/CapFactory.php
+++ database/factories/CapFactory.php
@@ -4,14 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Cap::class, function (Faker $faker) {
 
-    $img_id = (int)$faker->numberBetween($min = 1, $max = 10);
+    $img_id = (int)$faker->numberBetween($min = 1, $max = 40);
 
     return [
         // 'name' => $faker->name,
         'name' => $faker->name,
         'description' => $faker ->text,
         'price' => $faker->randomNumber(2),
-        'image' => '//lorempixel.com/640/480/people/'.$img_id.'/',
+        'image' => 'https://picsum.photos/400/300?image='.$img_id,
         'category_id' => factory('App\Models\Category')->create()->id,
         'supplier_id' => factory('App\Models\Supplier')->create()->id,
     ];
