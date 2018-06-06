6ce6a5785dccc6cc750d9e5d48cc1c838a509751
tim-hub <tim_w@outlook.com>
2018-06-06 15:23:03 +1200

    add people to random image placeholder url

--- database/factories/CapFactory.php
+++ database/factories/CapFactory.php
@@ -3,12 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Cap::class, function (Faker $faker) {
+
+    $img_id = (int)$faker->numberBetween($min = 1, $max = 10);
+
     return [
         // 'name' => $faker->name,
         'name' => $faker->name,
         'description' => $faker ->text,
-        'image' => $faker-> imageUrl($width = 640, $height = 480),
         'price' => $faker->randomNumber(2),
+        'image' => '//lorempixel.com/640/480/people/'.$img_id.'/',
         'category_id' => factory('App\Models\Category')->create()->id,
         'supplier_id' => factory('App\Models\Supplier')->create()->id,
     ];
