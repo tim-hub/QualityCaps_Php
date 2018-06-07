4234667d5d98c634ab5a2d916b17584bd3ad1a75
tim-hub <tim_w@outlook.com>
2018-06-07 20:49:48 +1200

    change style in front of fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,12 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-
-
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 
