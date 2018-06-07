82f55468e1ea9d49849a2685eca38dfb5139a841
tim-hub <tim_w@outlook.com>
2018-06-07 20:53:29 +1200

    add about page

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <iframe width="560" height="315"
+                    src="https://www.youtube-nocookie.com/embed/8Vs_yKsn2no"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-3"></div>
+
+    </div>
+    <br />
+    <br />
+    <br />
+    <div class="text-center">
+
+        <strong>Just provide the best for you! </strong>
+        <br />
+
+        <p>
+            Quanlity Caps is the caps outlet company base on Auckland, New Zealand.
+        </p>
+        <h4>About Caps Pictures</h4>
+        <p>
+            The cap pictures are collected from google search engine, they are only used for demostration for this assignment in Unitec.
+        </p>
+    </div>
+</div>
+@endsection
