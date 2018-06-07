639b1a78d5dbfa8bb241e1ff997a4ba114b277ea
tim-hub <tim_w@outlook.com>
2018-06-07 20:48:58 +1200

    add a footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="row col-lg-12">
+        <div class="col-md-6 col-sm-6">
+            <h5 class="text-center"><strong> &copy; 2018 - Quality Caps</strong></h5>
+            <p class="text-center">Just provide the best for you!</p>
+            <p class="small text-center">Quanlity Caps is the caps outlet company base on Auckland, New Zealand. </p>
+        </div>
+
+        <div class="col-md-6 col-sm-6">
+            <h5 class="text-center"><strong> Contact Us</strong></h5>
+            <p class="text-center small"> 9am to 5pm (Monday to Friday)</p>
+            <p class="text-center small">Room 2020 Level 2, Building 180, 139 Carrington Rd, Mount Albert, Auckland 1025</p>
+            <p class="text-center small text-muted"><a href="https://github.com/tim-hub">#Tim</a></p>
+        </div>
+    </div>
+
+</footer>
