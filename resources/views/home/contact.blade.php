63d5567b67f7d48e0621157080c6940af731a260
tim-hub <tim_w@outlook.com>
2018-06-07 20:57:18 +1200

    add contact page

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="body-content text-center">
+        <h3>Contact Us</h3>
+
+        <address>
+
+            9am to 5pm (Monday to Friday)<br />
+
+            Room 2020 Level 2, Building 180, 139 Carrington Rd, Mount Albert, Auckland 1025<br />
+
+            <abbr title="Gtihub">Website:</abbr>
+            <a href="https://github.com/tim-hub">Github</a>
+
+            <br/>
+            <abbr title="About">About:</abbr>
+            <a href="/Home/About">More</a>
+        </address>
+
+    </div>
+</div>
+@endsection
