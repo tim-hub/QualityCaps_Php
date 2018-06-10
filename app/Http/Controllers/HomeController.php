2eb5b8b05619e2bb1327b5efaf9e13749732ba76
tim-hub <tim_w@outlook.com>
2018-06-10 20:53:19 +1200

    add auth in home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
     public function __construct()
     {
         // $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['index', 'about', 'contact']]);
     }
 
     /**
@@ -43,4 +44,8 @@ class HomeController extends Controller
     {
         return view('home.contact');
     }
+
+
+
+
 }
