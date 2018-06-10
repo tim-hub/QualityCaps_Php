6b79d335a539b673d3ae1916762bdcc5d0dc48c0
tim-hub <tim_w@outlook.com>
2018-06-10 18:52:29 +1200

    UI basically done on detail page, need a shopping cart

--- resources/views/caps/show.blade.php
+++ resources/views/caps/show.blade.php
@@ -4,45 +4,67 @@
 
 <div class="container">
     <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
+        <div class="panel panel-success">
             <div class="panel-heading">
-                <h1>Cap / Show #{{ $cap->id }}</h1>
+            <h3 class="panel-title">{{ $cap->name }} #{{$cap->id}}</h3>
             </div>
 
             <div class="panel-body">
                 <div class="well well-sm">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-6">
                             <a class="btn btn-link" href="{{ route('caps.index') }}"><i class="glyphicon glyphicon-backward"></i> Back</a>
                         </div>
-                        <div class="col-md-6">
+                        @if (!Auth::guest() && Auth::user()-> role >0)
+                        <div class="col-6">
                              <a class="btn btn-sm btn-warning pull-right" href="{{ route('caps.edit', $cap->id) }}">
                                 <i class="glyphicon glyphicon-edit"></i> Edit
                             </a>
                         </div>
+                        @endif
                     </div>
                 </div>
 
-                <label>Category_id</label>
-<p>
-	{{ $cap->category_id }}
-</p> <label>Supplier_id</label>
-<p>
-	{{ $cap->supplier_id }}
-</p> <label>Name</label>
-<p>
-	{{ $cap->name }}
-</p> <label>Description</label>
-<p>
-	{{ $cap->description }}
-</p> <label>Price</label>
-<p>
-	{{ $cap->price }}
-</p> <label>Image</label>
-<p>
-	{{ $cap->image }}
-</p>
+                <div class="row">
+                    <div class="col-md-5 col-lg-5">
+                        <img
+                        class="rounded float-left img-fluid"
+                        src="{{$cap->image}}" />
+                    </div>
+                    <div class="col-md-7 col-lg-7">
+                        <ul class="list-inline">
+                                <li class="list-inline-item badge badge-primary">
+                                        {{ $cap->supplier->name }}
+                                </li>
+
+                                <li class="list-inline-item badge badge-success">
+                                        {{ $cap->category->name }}
+                                </li>
+                            </ul>
+
+                            <hr>
+                            <p>
+                                {{ $cap->description }}
+                            </p>
+                            <hr>
+                            <h2>
+                                $<span
+                                class=""
+                                >
+                                {{ $cap->price }}
+                                </span>
+
+                                <button class="btn btn-success">
+                                    Add To Cart
+                                </button>
+                            </h2>
+
+                    </div>
+
+                </div>
+
             </div>
+            <div class="panel-footer"></div>
         </div>
     </div>
 </div>
