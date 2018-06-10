fe827d2dce2fd5ac4d48f2b37a7e62eb789a3a77
tim-hub <tim_w@outlook.com>
2018-06-10 17:03:57 +1200

    try to get video position

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -3,19 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="col-1 col-md-3 col-sm-2 "></div>
+        <div class="col-10 col-md-6 col-sm-8 ">
             <iframe width="560" height="315"
                     src="https://www.youtube-nocookie.com/embed/8Vs_yKsn2no"
                     frameborder="0"
                     allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
-        <div class="col-md-3"></div>
+        <div class="col-1 col-md-3 col-sm-2 "></div>
 
     </div>
     <br />
     <br />
     <br />
+
     <div class="text-center">
 
         <strong>Just provide the best for you! </strong>
