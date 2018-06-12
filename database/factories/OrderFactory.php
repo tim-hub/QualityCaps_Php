fc6f9c7cc6c286c8a1706e0c41c67792bb8fce18
tim-hub <tim_w@outlook.com>
2018-06-12 21:14:02 +1200

    order andi tems factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -5,5 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Order::class, function (Faker $faker) {
     return [
         // 'name' => $faker->name,
+        // 'quantity' => $faker ->randomNumber(2) ,
+        'receiver_name' =>$faker ->name,
+        'address' => $faker ->address,
+        'status' => $faker  ->numberBetween($min = 0, $max = 2),
+        'user_id' => $faker ->numberBetween ($min=2, $max=4),
     ];
 });
--- database/factories/Order_ItemFactory.php
+++ database/factories/Order_ItemFactory.php
@@ -5,5 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Order_Item::class, function (Faker $faker) {
     return [
         // 'name' => $faker->name,
+        'quantity' => $faker ->randomNumber(2),
+        'cap_id' => $faker  ->numberBetween($min = 1, $max = 10),
+        'order_id' => $faker ->numberBetween ($min=1, $max=10),
     ];
 });
