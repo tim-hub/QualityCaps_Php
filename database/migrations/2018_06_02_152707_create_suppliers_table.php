dba3e78454e9a8e69a6c0c0c3510812c81b30f24
tim-hub <tim_w@outlook.com>
2018-06-12 14:17:55 +1200

    description add nullable in category and supplier

--- database/migrations/2018_06_02_152707_create_suppliers_table.php
+++ database/migrations/2018_06_02_152707_create_suppliers_table.php
@@ -16,7 +16,7 @@ class CreateSuppliersTable extends Migration
         Schema::create('suppliers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',100);
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
