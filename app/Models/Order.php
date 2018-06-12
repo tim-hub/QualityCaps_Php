2e3d6128dcb0113a3c522a98c0d009243a923152
tim-hub <tim_w@outlook.com>
2018-06-12 21:14:33 +1200

    order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,32 +9,31 @@ class Order extends Model
     protected $table = 'orders';
     /**
      * The attributes that are mass assignable.
-     *
+     *  status 0 ,1 ,2 waiting shipping and done
      * @var array
      */
     protected $fillable = [
-        'user_id', 'status', 'quantity', 'receiver_name', 'address'
+        'user_id', 'status', 'receiver_name', 'address'
     ];
 
     public function user() {
-        return $this->belongsTo('User');
+        return $this->belongsTo('App\User');
     }
     public function order_items() {
-        return $this->hasMany('Order_Item');
+        return $this->hasMany('App\Models\Order_Item');
     }
 
     public function grand_total(){
 
-        $order_items = order_items();
+        $order_items = $this->order_items();
         $sum =0;
 
         foreach($order_items as $item){
-            $sum += $item->sub_total;
+            $sum += $item->sub_total();
         }
         return $sum;
-
     }
     public function grand_total_with_gst(){
-        return grand_total() * $this ->gst;
+        return $this->grand_total() * (1+$this ->gst);
     }
 }
