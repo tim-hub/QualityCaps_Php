d4c11ba1bb6f84a541f44085cfc9fca17d8d6f76
tim-hub <tim_w@outlook.com>
2018-06-13 12:05:37 +1200

    add darryldecode/cart

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Darryldecode\Cart\CartServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class, //cart
 
     ],
 
