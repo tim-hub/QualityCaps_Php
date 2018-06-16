a105fc65d99bf22e37aa32ea0d0aab04bab52503
tim-hub <tim_w@outlook.com>
2018-06-16 18:49:08 +1200

    now admin cannot porcess an order

--- resources/views/carts/add.blade.php
+++ resources/views/carts/add.blade.php
@@ -1,4 +1,6 @@
 
+{{--if user is administrator, cannot process an order--}}
+@if (!Auth::guest() && Auth::user()-> role <9)
 <form action="{{route('carts.store')}}" method="POST">
     {!! csrf_field() !!}
     <input type="hidden" name="id" value="{{ $cap->id }}">
@@ -6,3 +8,5 @@
     <input type="hidden" name="price" value="{{ $cap->price }}">
     <input type="submit" class="btn btn-success btn-lg" value="Add to Cart">
 </form>
+
+@endif
